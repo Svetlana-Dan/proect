806485ee4cf74c8e3b566ba521eb59b4238e1e76
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-06-09 14:02:24 +0800

    Update datas.php

--- проект/datas.php
+++ проект/datas.php
@@ -1,9 +1,11 @@
 <?php
-    include_once 'classes/database.php';
+    include_once 'classes/database.php';	
+		$now_editing_task_id = -1; 
     $topics = Database::exec("SELECT * FROM `topics`");
     $durations = Database::exec("SELECT * FROM `durations`");
     $if_doned =  Database::exec("SELECT * FROM `if_doned`");
     $data_rel = false;
+		$now_editing_task_status = null;
     $data = null;
     $message = null;
     $what_search = array(
